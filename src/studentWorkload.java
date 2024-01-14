12403dcbbb3f8ce6e9a91f0f87a8c783ac7fb8e9
JMorey-1 <152331168+JMorey-1@users.noreply.github.com>
2024-01-14 21:03:44 +0000

    Update studentWorkload.java

--- src/studentWorkload.java
+++ src/studentWorkload.java
@@ -9,42 +9,54 @@ import java.io.BufferedWriter;
  */
 public class studentWorkload {
 
-    public static void main(String[] args) {
-           Scanner scanner = new Scanner(System.in);
-         
-           
-               System.out.println("Please choose an option to continue:");
-               System.out.println("Please press 1 to output data to file");
-               System.out.println("Please press 2 to add data to file");
-               System.out.println("Please press 3 to exit program");
-               
-               int option;
-               option = scanner.nextInt();
-               
-               switch (option){
-                       case 1: 
-                            System.out.println("You choose option 1");
-                            System.out.println("Do you want to return to the main menu? (y/n)");
-                             String answer = scanner.next();
-                             if (answer.equals("y"))
-                            break;
-                       
-                       case 2:
-                            System.out.println("You choose option 2");
-                            System.out.println("Please enter the data you would like to add to the Student file?");
-                            System.out.println("");
-                            break;
-                       
-                       case 3:
-                            System.out.println("Exiting the program");
-                            break;
-                            
-                       default:
-                            System.out.println("Invalid choice please enter 1, 2 or 3.");
-                            break;
-                              
-                          }
-                        
+         public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            
+            while (true) {
+            int option = menu();
+
+            switch (option) {
+                case 1:
+                    System.out.println("Student data has been output to file.");
+                    break;
+
+                case 2:
+                    System.out.println("Please enter the data you would like to add to the Student file?");
+                    break;
+
+                case 3:
+                    System.out.println("Exiting the program");
+                    System.exit(0);
+
+                default:
+                    System.out.println("Invalid choice please enter 1, 2, or 3.");
+                    break;
+                   }
+            
+                   System.out.println("Do you want to return to the main menu? (y/n)");
+                   String answer = scanner.next();
+            
+                   if (!answer.equals("y")) {
+                   System.out.println("Exiting the program");
+                   System.exit(0);
+                   }
+     }
+   }
+    //MENU METHOD
+    public static int menu() {
+
+        int selection;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please choose an option to continue:");
+        System.out.println("------------------------------------");
+        System.out.println("Please press 1 to output data to file");
+        System.out.println("Please press 2 to add data to file");
+        System.out.println("Please press 3 to exit program");
+
+        selection = input.nextInt();
+        return selection;    
+    }
+
     
-   }      
  }
